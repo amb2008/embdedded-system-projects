4a18397558b96aace9d36b3c27e43fb43f302959
amb2008 <113376250+amb2008@users.noreply.github.com>
2023-07-22 19:33:24 -0400

    Create beginnerIot.ino

---
+++ beginnerIot.ino
@@ -0,0 +1,126 @@
+#define BLYNK_TEMPLATE_ID "blynk_template_ID"
+
+#define BLYNK_DEVICE_NAME "blynk_device_name"
+
+#define BLYNK_AUTH_TOKEN "blynk_auth_token"
+
+
+
+#include <ESP8266WiFi.h>
+
+#include <BlynkSimpleEsp8266.h>
+
+
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+
+
+// Your WiFi credentials.
+
+// Set password to "" for open networks.
+
+char ssid[] = "wifi name";
+
+char pass[] = "wifi password";
+
+
+
+BlynkTimer timer;
+
+
+
+// This function is called every time the Virtual Pin 0 state changes
+
+BLYNK_WRITE(V0)
+
+{
+
+// Set incoming value from pin V0 to a variable
+
+int value = param.asInt();
+
+
+
+// Update state
+
+Blynk.virtualWrite(V1, value);
+
+digitalWrite(D0, value);
+
+}
+
+
+
+// This function is called every time the device is connected to the Blynk.Cloud
+
+BLYNK_CONNECTED()
+
+{
+
+// Change Web Link Button message to "Congratulations!"
+
+Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+
+Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+
+Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
+
+}
+
+
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+
+void myTimerEvent()
+
+{
+
+// You can send any value at any time.
+
+// Please don't send more that 10 values per second.
+
+Blynk.virtualWrite(V2, millis() / 1000);
+
+}
+
+
+
+void setup()
+
+{
+
+// Debug console
+
+Serial.begin(115200);
+
+pinMode(D0, OUTPUT);
+
+
+
+Blynk.begin(auth, ssid, pass);
+
+
+
+// Setup a function to be called every second
+
+timer.setInterval(1000L, myTimerEvent);
+
+}
+
+
+
+void loop()
+
+{
+
+
+
+Blynk.run();
+
+timer.run();
+
+
+
+}
+
